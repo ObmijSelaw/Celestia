e75abb71edd561f4da3fef0a4c29abeff3157de0
ObmijSelaw <81539176+ObmijSelaw@users.noreply.github.com>
2026-01-31 21:37:26 -0600

    Update texture.cpp (reincorp from sRGB branch)

--- src/celengine/texture.cpp
+++ src/celengine/texture.cpp
@@ -74,6 +74,15 @@ getInternalFormat(PixelFormat format)
     case PixelFormat::DXT3:
     case PixelFormat::DXT5:
         return static_cast<GLenum>(format);
+    case PixelFormat::SRGB:
+    case PixelFormat::SRGB8:
+        return GL_SRGB8;
+    case PixelFormat::SRGBA:
+    case PixelFormat::SRGBA8:
+    case PixelFormat::DXT1_SRGBA:
+    case PixelFormat::DXT3_SRGBA:
+    case PixelFormat::DXT5_SRGBA:
+        return GL_SRGB8_ALPHA8;
     default:
         return GL_NONE;
     }
@@ -108,8 +117,18 @@ GLenum
 getExternalFormat(PixelFormat format)
 {
 #ifdef GL_ES
-    return getInternalFormat(format);
-#else
+    switch (format)
+    {
+        case PixelFormat::SRGB:
+        case PixelFormat::SRGB8:
+            return GL_RGB;
+        case PixelFormat::SRGBA:
+        case PixelFormat::SRGBA8:
+            return GL_RGBA;
+        default:
+            return getInternalFormat(format);
+    }
+    #else
     switch (format)
     {
     case PixelFormat::RGBA:
@@ -268,7 +287,7 @@ void
 LoadMiplessTexture(const Image& img, GLenum target)
 {
     int internalFormat = getInternalFormat(img.getFormat());
-
+    GetLogger()->info("Format, internal: {:X}, external {:X}\n", internalFormat, getExternalFormat(img.getFormat()));
     if (img.isCompressed())
     {
         glCompressedTexImage2D(target,
@@ -281,6 +300,7 @@ LoadMiplessTexture(const Image& img, GLenum target)
     }
     else
     {
+        while (glGetError() != 0);
         glTexImage2D(target,
                      0,
                      internalFormat,
@@ -289,6 +309,9 @@ LoadMiplessTexture(const Image& img, GLenum target)
                      getExternalFormat(img.getFormat()),
                      GL_UNSIGNED_BYTE,
                      img.getMipLevel(0));
+        
+        if (internalFormat >= 0x8c40 && internalFormat <= 0x8c43)
+            GetLogger()->info("GL error: {:X}\n", glGetError());
     }
 }
 
