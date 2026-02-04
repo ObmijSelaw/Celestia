0ba26ac9f2fedc3faf427b3c833dc4b3c50e4ba6
ObmijSelaw <81539176+ObmijSelaw@users.noreply.github.com>
2026-02-04 03:25:20 -0600

    Update star_frag.glsl (Update PSF core from 2ca7268 from askaniy)

--- shaders/star_frag.glsl
+++ shaders/star_frag.glsl
@@ -1,6 +1,5 @@
-const float degree_per_px = 0.05;
+const float degree_per_px = 0.01;
 const float br_limit = 1.0 / (255.0 * 12.92);
-
 // empirical constants
 const float a = 0.123;
 const float k = 0.0016;
@@ -8,51 +7,36 @@ const float k = 0.0016;
 varying vec3 v_color;
 varying float max_theta;
 varying float pointSize;
-varying float br;
 
-// py: def PSF_Bounded(theta: float, max_theta: float, br_center: float):
-// max_theta is common for all pixels so it's set via `varying` in the vertex shader
-
-float psf_bounded(float theta, float br_center)
+float psf_core(float offset)
 {
-    // Human eye's point source function from the research by Greg Spencer et al., optimized to fit a square.
-    // Lower limit on brightness and angular size: 1 Vega and 0.05 degrees per pixel. No upper limits.
+    // Human eye's point source function from the research by Greg Spencer et al. (1995)
+    // Optimized for the central part of the PSF. The flow from the nine neighboring pixels is constant.
+    // Designed for degree_per_px == 0.01.
+    return 1.0 + offset * (0.2789 * offset - 1.0);
+    // the second summand is allowed to be scaled to achieve a seamless transition between modes
+}
 
-    // py: if theta == 0:
+float psf_glow(float offset)
+{
+    // Human eye's point source function from the research by Greg Spencer et al. (1995)
+    // Optimized for the outer part of the PSF. Designed with bounds by arctangent in mind.
+    // Causes star blinking with degree_per_px > 0.01, large grid misses the center peak of brightness.
+    float theta = offset * degree_per_px;
     if (theta == 0)
-    // py:  return br_center
-        return br_center; // the center is always overexposed (zero division error)
-    // py:  elif theta < max_theta:
+        return 100.; // the center is always overexposed (zero division error)
     if (theta < max_theta)
     {
         float brackets = max_theta / theta - 1.0;
         return k * brackets * brackets;
     }
-
-    return 0.0;
+    return 0.0; // after max_theta function starts to grow again
 }
 
 void main(void)
 {
-    vec3 glow_colored;
-    if (max_theta == -1.0)
-    {
-        // just a one pixel star
-        // py: arr[center[1], center[0]] += scaled_color
-        gl_FragColor = vec4(v_color, 1.0);
-    }
-    else
-    {
-        float min_theta = v_tetha_k.y;
-        float k = v_tetha_k.z;
-        // Option 2: glare square render
-        vec2 offset = (gl_PointCoord.xy - vec2(0.5)) * pointSize;
-        float theta = length(offset) * degree_per_px;
-        // py: glow_bw = PSF_Bounded(theta, max_theta, br) # in the [0, 1] range, like in Celestia
-        float glow_bw = psf_bounded(theta, br0);
-        // py: glow_colored = color * np.repeat(np.expand_dims(glow_bw, axis=2), 3, axis=2) # scaling
-        vec3 glow_colored = v_color * glow_bw;
-        // py: arr[center[1]+y_min:center[1]+y_max, center[0]+x_min:center[0]+x_max] += glow_colored
-        gl_FragColor = vec4(glow_colored, 1.0);
-    }
+    // in fragment shader all points have virtual dimension 1x1, so gl_PointCoord has a value from [0; 1]
+    float offset = length((gl_PointCoord.xy - vec2(0.5)) * pointSize);
+    float black_and_white = (max_theta == -1.0) ? psf_core(offset) : psf_glow(offset);
+    gl_FragColor = vec4(v_color * black_and_white, 1.0); // + vec4(0.1, 0.0, 0.0, 0.0); // square for debugging
 }
